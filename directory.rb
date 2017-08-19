6d9b7e1269997b721467fd323484ffe8477f6af2
James Hughes <hugjimbo77@gmail.com>
2017-08-19 14:03:21 +0100

    Saving Data to a file

--- directory.rb
+++ directory.rb
@@ -1,8 +1,45 @@
+  @students = [] # an empty array accessible to all methods
+
+  def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Save the list to students.csv"
+    puts "9. Exit" # 9 because we'll be adding more items
+  end
+
+
+  def interactive_menu
+    loop do
+      print_menu
+      process(gets.chomp)
+    end
+  end
+
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
+end
+
+def push_to_students(name, cohort, height, eyecolour)
+  @students << {name: name, cohort: cohort, height: height, eyecolour: eyecolour}
+end
+
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  students = []                                     # create an empty array
   name = gets.strip                                 # get the first name
   while !name.empty? do                             # while the name is not empty, repeat this code
     puts "Which cohort have they been placed on?"
@@ -16,15 +53,21 @@ def input_students
     eyecolour = gets.strip
 
     # add the student hash to the array
-    students << {name: name, cohort: cohort, height: height, eyecolour: eyecolour}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort, height: height, eyecolour: eyecolour}
+    puts "Now we have #{@students.count} students"
     puts "Please enter the details of more students"
     # get another name from the user
     name = gets.strip
   end
-  # return the array of students
-  students
 end
+
+
+def show_students
+  print_header
+  student_selector
+  print_footer
+end
+
 
 def print_header
   center_me("The students of Villains Academy")
@@ -32,8 +75,8 @@ def print_header
 end
 
 
-def student_selector(students)
-  students.each_with_index do |student, index|
+def student_selector
+  @students.each_with_index do |student, index|
    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0].capitalize == "J" && student[:name].length < 12
   end
 end
@@ -45,11 +88,11 @@ def center_me(string)
 end
 
 #footer to print singular or plural students
-def print_footer(names)
-  unless names.count == 1
-    puts "Overall, we have #{names.count} great students.".center(50)
+def print_footer
+  if @students.count == 1
+    puts "Overall, we have a great student".center(50)
   else
-    puts "Overall, we have #{names.count} great student.".center(50)
+    puts "Overall, we have #{@students.count} great students".center(50)
   end
 end
 
@@ -64,5 +107,18 @@ def none_enrolled(students)
   end
 end
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+interactive_menu
 students = input_students
 none_enrolled(students)
